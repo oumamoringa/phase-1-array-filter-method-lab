b34be2253383143f01be9772a1812f8d585a7969
oumamoringa <richard.ouma@student.moringaschool>
2022-08-20 18:44:21 +0300

    push

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const drivers = ["Richie", "Jane", "Mary",]
+                ["Brian", "Sammy", "Oti"  ]
+
+
+//returns all drivers that match  the passed in name
+const findMatching =(p,q) => p.filter(p => p.toLowerCase() === q.toLowerCase())
+
+//returns a driver if beginning provided letters match
+const fuzzyMatch = (p,q) => p.filter(p => p.charAt(0) === q.charAt(0))
+
+//acces the data structure to check if the name match
+const matchName = (p, q) => p.filter(p => p.name === q)
